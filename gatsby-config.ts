c91149046a465dd211cfbde9185935d41b8a7979
Alen Rubilar <lclc.alen@gmail.com>
2022-05-26 00:30:33 -0400

    Lorem ipsum

---
+++ gatsby-config.ts
@@ -0,0 +1,31 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Without Mask Society`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Without Mask Society",
+        short_name: "WMS",
+        start_url: "/",
+        background_color: "#000000",
+        theme_color: "#4ade80",
+        display: "standalone",
+        icon: "src/images/favicon.jpg",
+      },
+    },
+    "gatsby-transformer-remark",
+  ],
+};
+
+export default config;
